c2c04b1c6a653362714b4dd4e6415a4d11605f0b
tpwiedy <wiedy.pratama@gmail.com>
2024-05-03 08:35:01 +0700

    bugfixing: add error middleware

--- index.js
+++ index.js
@@ -1,44 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const bcrypt = require('bcrypt');
-const passport = require('./libs/passport')
-const fs = require('fs');
-const morgan = require('morgan');
-const path = require('path');
-const routerIndex = require('./src/routes/index-router');
 
+const routerIndex = require('./src/routes/index-router');
+const { notFound, httpEvent } = require('./src/middlewares/error-middleware');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize())
-
-
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' },
-);
-
 
 // API ROUTERS
 app.use(routerIndex);
 
-
-// Morgan Log
-app.use(
-  morgan('combined', {
-    stream: accessLogStream,
-    skip: function (req, res) {
-      return res.statusCode < 400;
-    },
-  }),
-);
-
+// ERROR HANDLERS
+app.use(notFound);
+app.use(httpEvent);
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
 });
-
-
---
+++ src/middlewares/index.js
@@ -0,0 +1,1 @@
+module.exports = { isAuthenticated: require('./isAuthenticated') };
