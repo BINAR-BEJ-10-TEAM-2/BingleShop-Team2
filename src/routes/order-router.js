30dae3a648ca242698be3fa303e675745b64c87b
Fadhil Kasvi <fadhilkasvi@gmail.com>
2024-05-05 00:37:27 +0700

    Tambahan [Autorization] GET orderList all untuk admin

--- src/routes/order-router.js
+++ src/routes/order-router.js
@@ -1,11 +1,12 @@
 const orderRouter = require('express').Router();
 const orderController = require('../controller/order-controller');
+const { is_admin } = require('../middlewares');
 
 orderRouter.post('/:userId/create-order', orderController.createOrder);
 orderRouter.get('/:orderId', orderController.getOrder);
 orderRouter.put('/:orderId/completed', orderController.putOrder);
 
 // Admin
-orderRouter.get('/admin/order-list', orderController.getListOrder);
+orderRouter.get('/admin/order-list', is_admin, orderController.getListOrder);
 
 module.exports = orderRouter;
