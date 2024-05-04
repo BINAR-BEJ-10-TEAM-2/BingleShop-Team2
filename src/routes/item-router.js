d62ea1352a62f1a283883d621917e399507afd09
exwn <ixwanull.4za@outlook.com>
2024-05-04 23:28:35 +0700

    [BST2-47] refactor: item-route path

--- src/routes/item-router.js
+++ src/routes/item-router.js
@@ -1,8 +1,12 @@
 const itemRouter = require('express').Router();
 const itemController = require('../controller/item-controller');
+const { is_admin } = require('../middlewares');
 
-itemRouter.post('/', itemController.createItem);
-itemRouter.get('/', itemController.getItem);
-itemRouter.put('/:itemId', itemController.updateItem);
+
+itemRouter.post('/admin/add-item', is_admin, itemController.createItem);
+itemRouter.get('/list', itemController.getItem);
+itemRouter.put('/admin/update-item/:itemId', is_admin, itemController.updateItem);
+itemRouter.get('/admin/specified-item/:itemId', is_admin, itemController.getItemById);
+itemRouter.delete('/admin/delete-item/:itemId', is_admin, itemController.deleteItemById);
 
 module.exports = itemRouter;
