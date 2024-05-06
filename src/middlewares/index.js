719511f46e6099a38def78a5d2ed6d294c33e1c0
tpwiedy <wiedy.pratama@gmail.com>
2024-05-06 18:47:25 +0700

    update: adding isAdmin middleware in Get All Order endpoint

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,6 +1,11 @@
+const isAuthenticated = require('./isAuthenticated');
+const errorNotFound = require('./errorNotFound');
+const errorHttpEvent = require('./errorHttpEvent');
+const isAdmin = require('./isAdmin');
+
 module.exports = {
-  isAuthenticated: require('./isAuthenticated'),
-  errorNotFound: require('./errorNotFound'),
-  errorHttpEvent: require('./errorHttpEvent'),
-  isAdmin: require('./isAdmin'),
+  isAuthenticated,
+  errorNotFound,
+  errorHttpEvent,
+  isAdmin,
 };
