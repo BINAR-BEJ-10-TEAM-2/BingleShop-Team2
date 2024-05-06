ce7d40cb6c570b8dc5ad9ee08b51ccec4de489fa
tpwiedy <wiedy.pratama@gmail.com>
2024-05-06 20:19:47 +0700

    update: change authorization header to bearer token

--- src/libs/passport.js
+++ src/libs/passport.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+
 const { JWT_SECRET_KEY } = process.env;
 const { User } = require('../models');
 
 passport.use(
   new JwtStrategy(
     {
-      jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: JWT_SECRET_KEY,
     },
     (payload, done) => {
