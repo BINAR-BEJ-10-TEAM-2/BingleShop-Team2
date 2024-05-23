42e0435813e3060b4649db18bf337fa3ad72e29d
tpwiedy <wiedy.pratama@gmail.com>
2024-05-23 10:03:31 +0700

    adding unhandler rejection and sigterm handler

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   logger.info(`Server app listening on http://localhost:${port}`);
 });
+
+process.on('unhandledRejection', (err) => {
+  logger.info('UNHANDLER REJECTION! 💥 Shutting down...');
+  logger.info(err.name, err.message);
+  app.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  logger.info('👏 SIGTEM RECEIVED. Shutting down gracefully');
+  app.close(() => {
+    logger.info('💥 Process terminated!');
+  });
+});
