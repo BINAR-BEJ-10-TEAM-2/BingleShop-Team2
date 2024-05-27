95928379d46c6f7098a94bfa8b2357fb5f09fa96
nancy <nancypgenia@gmail.com>
2024-05-27 21:21:57 +0700

    [BST2-80] adding form validation in item-create and item-update route

--- src/routes/item-router.js
+++ src/routes/item-router.js
@@ -1,11 +1,11 @@
 const itemRouter = require('express').Router();
 const itemController = require('../controller/item-controller');
 const { uploadItemImage } = require('../libs/multer');
-const { isAdmin } = require('../middlewares');
+const { isAdmin, validateRequest } = require('../middlewares');
 
-itemRouter.post('/admin/add-item', isAdmin, uploadItemImage, itemController.createItem);
+itemRouter.post('/admin/add-item', isAdmin, uploadItemImage, validateRequest.itemCreate , itemController.createItem);
 itemRouter.get('/list', itemController.getItem);
-itemRouter.put('/admin/update-item/:itemId', isAdmin, uploadItemImage, itemController.updateItem);
+itemRouter.put('/admin/update-item/:itemId', isAdmin, uploadItemImage, validateRequest.itemUpdate, itemController.updateItem);
 itemRouter.get('/admin/specified-item/:itemId', isAdmin, itemController.getItemById);
 itemRouter.delete('/admin/delete-item/:itemId', isAdmin, itemController.deleteItemById);
 
