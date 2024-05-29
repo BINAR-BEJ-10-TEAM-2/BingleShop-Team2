c69a30bb2987baf156d70c61b8e9b6e0b126aa7e
tpwiedy <wiedy.pratama@gmail.com>
2024-05-30 00:12:02 +0700

    fixing integration testing createItem with adding condition while in 'test' env

--- test/integration/item-test/createItem.test.js
+++ test/integration/item-test/createItem.test.js
@@ -12,9 +12,7 @@ const {
 const database = require('../../helpers/database');
 
 describe('POST /api/items/admin/add-item', () => {
-  // const filePath = ${__dirname}/uploads/image.png
   const filePath = path.resolve(__dirname, '../../../uploads/image.png');
-  // console.log(filePath)
 
   beforeEach(async () => {
     await database.cleanup();
@@ -32,14 +30,12 @@ describe('POST /api/items/admin/add-item', () => {
       .field('description', 'Lorem Ipsum')
       .attach('item_image', filePath);
 
-    // console.log('Response Body:', response.body);
     expect(response.status).toBe(201);
     expect(response.body.message).toBeDefined();
   });
 
   it('should return an error when the user is not admin', async () => {
     const token = await createTestUser();
-    // const token = await createTestUserAdmin();
 
     const response = await supertest(app)
       .post('/api/items/admin/add-item')
@@ -50,7 +46,6 @@ describe('POST /api/items/admin/add-item', () => {
       .field('description', 'Lorem Ipsum')
       .attach('item_image', filePath);
 
-    // console.log('Response Body:', response);
     expect(response.status).toBe(403);
     expect(response.body.message).toBe('Forbidden');
   });
